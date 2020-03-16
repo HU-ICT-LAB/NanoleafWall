e9b7e4ba4294d4ab841f55151c6c5c175f471bdd
Oussama <oussama.ameziane@student.hu.nl>
2020-03-16 20:40:33 +0100

    Test bestand voor effecten lijst + kleuren

---
+++ Test.py
@@ -0,0 +1,27 @@
+from nanoleafapi import discovery, Nanoleaf
+from nanoleafapi import RED, ORANGE, YELLOW, GREEN, LIGHT_BLUE, BLUE, LIGHT_BLUE, PINK, PURPLE, WHITE
+from flask import Flask
+
+app = Flask(__name__)
+
+auth_token = "yD3RlM2LKIBAUJXxILjnjeBIKtxLGroB"
+ip = "192.168.3.34"
+
+nl = Nanoleaf(ip, auth_token)
+
+#nl.set_color(PURPLE)
+#nl.set_color((210,105,30))
+
+x = nl.list_effects()
+print (x)
+
+effects = ['Color Burst', 'Falling Whites', 'Fireworks', 'Flames', 'Forest', 'Inner Peace', 'Meteor Shower', 'Nemo', 'Northern Lights', 'Paint Splatter', 'Pulse PopBeats', 'Radial Sound Bar', 'Rhythmic Northern Lights', 'Romantic', 'Sound Bar', 'Streaking Notes']
+ 
+nl.set_effect(effects[0])
+
+y = nl.get_current_effect()
+print (y)
+
+@app.route("/effecten" , methods = ["get"])
+def effect_option():
+	return print (x)
