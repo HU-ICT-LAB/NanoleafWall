52ce81b75ac4b5e9d7689679ed28a43bbc9d063f
nand <nandhoman@gmail.com>
2020-04-21 00:59:45 +0200

    Mocking2

---
+++ mocking2be/generatecolorstring.js
@@ -0,0 +1,18 @@
+function generatecolorstring(){
+    var totalpanels;
+    var anidata = "103 ";
+
+    for (totalpanels = 1; totalpanels < 103; totalpanels++) {
+        var role1 = totalpanels.toString() + " ";
+        var role2 = "1 "
+        var role3 = 25 + " ";
+        var role4 = 0 + " ";
+        var role5 = 0 + " ";
+        var role6 = 0 + " ";
+        var role7 = "200 ";
+        anidata = anidata + role1 + role2 + role3 + role4 + role5 + role6 + role7;
+    }
+return anidata
+}
+
+console.log(generatecolorstring());
