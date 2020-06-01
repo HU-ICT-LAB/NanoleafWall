a1c63f08c7d69484960c88c59fdd1f1c3a13c5f3
nand <nandhoman@gmail.com>
2020-06-01 22:00:54 +0200

    version 1
    features: select players, select game, first animation

--- menu.py
+++ menu.py
@@ -11,6 +11,18 @@ def background(r, g, b):
   for id in range(102):
     toAppend = str((id + 1)) + " " + "1 " + r + " " + g + " " + b + " 0 200"
     color_array.append(toAppend)
+  return "done"
+
+def background2(r, g, b):
+  for id in range(102):
+    toAppend = str((id + 1)) + " " + "1 " + r + " " + g + " " + b + " 0 200"
+    color_array[id] = toAppend
+    for row in range(17):
+      idnum = 6 * row
+      if id == idnum:
+        time.sleep(0.1)
+        Send_module(color_array)
+  return "done"
 
 def Send_module(color_array):
   colorstring = "102"
@@ -50,63 +62,85 @@ def init_text(string):
     if every.find("\"panelId\"") != -1:
       newevery = every.replace("\"", "").replace("panelId:", "")
       datarray.append(newevery)
-      print(newevery)
   return datarray
 
 def touchComparison(wanted, input):
   equal = []
   for every in wanted:
-    for num in range((len(input) + 1)):
-      print(every, num)
+    for num in range(len(input)):
       if input[num] == every:
         equal.append(every)
   return equal
 
 def game_select_view1():
-  background("230", "230", "250")
-  print(color_array)
-  Send_module(color_array)
   toAppend = str(62) + " " + "1 " + "255" + " " + "16" + " " + "16" + " 0 200"
   color_array[62] = toAppend
   Send_module(color_array)
+  time.sleep(0.2)
   toAppend = str(65) + " " + "1 " + "6" + " " + "97" + " " + "255" + " 0 200"
-  print(color_array)
   color_array[65] = toAppend
   Send_module(color_array)
+  time.sleep(0.2)
   toAppend = str(80) + " " + "1 " + "245" + " " + "223" + " " + "26" + " 0 200"
   color_array[80] = toAppend
   Send_module(color_array)
+  time.sleep(0.2)
   toAppend = str(83) + " " + "1 " + "64" + " " + "162" + " " + "67" + " 0 200"
   color_array[83] = toAppend
   Send_module(color_array)
+  return "done"
 
 def player_count_select_view1():
   toAppend = str(15) + " " + "1 " + "245" + " " + "223" + " " + "26" + " 0 200"
   color_array[15] = toAppend
-  Send_module(color_array)
   toAppend = str(33) + " " + "1 " + "6" + " " + "97" + " " + "255" + " 0 200"
   color_array[33] = toAppend
-  Send_module(color_array)
   toAppend = str(40) + " " + "1 " + "6" + " " + "97" + " " + "255" + " 0 200"
   color_array[40] = toAppend
-  Send_module(color_array)
   toAppend = str(58) + " " + "1 " + "64" + " " + "162" + " " + "67" + " 0 200"
   color_array[58] = toAppend
-  Send_module(color_array)
   toAppend = str(63) + " " + "1 " + "64" + " " + "162" + " " + "67" + " 0 200"
   color_array[63] = toAppend
-  Send_module(color_array)
   toAppend = str(70) + " " + "1 " + "64" + " " + "162" + " " + "67" + " 0 200"
   color_array[70] = toAppend
   Send_module(color_array)
-  
+  return "done"
 
-background("230", "230", "250")
-Send_module(color_array)
-player_count_select_view1()
-time.sleep(10)
-game_select_view1()
-# print(touchComparison([3, 0, 2, 5, 70], init_text(tilelistener())))
+
+def main():
+  background("230", "230", "200")
+  Send_module(color_array)
+  player_count_select_view1()
+  touchedTheWriteOne = False
+  while touchedTheWriteOne == False:
+    touchInput = init_text(tilelistener())
+    print(touchInput)
+    RightOneTouchedP = touchComparison(['15', '33', '40', '58', '63', '70'], touchInput)
+    if len(RightOneTouchedP) > 0:
+      touchedTheWriteOne = True
+  time.sleep(0.5)
+  background2("230", "200", "200")
+  time.sleep(0.2)
+  game_select_view1()
+  touchedTheWriteOne = False
+  while touchedTheWriteOne == False:
+    touchInput = init_text(tilelistener())
+    print(touchInput)
+    RightOneTouchedG = touchComparison(['62', '65', '80', '83'], touchInput)
+    if len(RightOneTouchedG) > 0:
+      touchedTheWriteOne = True
+  if RightOneTouchedG == ['62']:
+    print("62")
+  if RightOneTouchedG == ['65']:
+    print("65")
+  if RightOneTouchedG == ['80']:
+    print("80")
+  if RightOneTouchedG == ['83']:
+    print("83")
+
+main()
+
+
 
 
 
